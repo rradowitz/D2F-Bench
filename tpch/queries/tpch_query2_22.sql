9469d106651bee6bb6c07660a2e0327b85703d00
rradowitz <raphaelradowitz@googlemail.com>
2017-09-08 02:50:39 +0200

    update q 22

--- tpch/queries/tpch_query2_22.sql
+++ tpch/queries/tpch_query2_22.sql
@@ -10,7 +10,7 @@ from (
 		customer
 	where
 		substring(c_phone from 1 for 2) in
-			('13','31’,'23','29','30','18','17')
+			('13','31','23','29','30','18','17')
 		and c_acctbal > (
 			select
 				avg(c_acctbal)
@@ -19,7 +19,7 @@ from (
 			where
 				c_acctbal > 0.00
 				and substring (c_phone from 1 for 2) in
-						('13','31’,'23','29','30','18','17')
+						('13','31','23','29','30','18','17')
 		)
 		and not exists (
 			select
